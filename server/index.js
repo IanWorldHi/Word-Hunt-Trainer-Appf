0f997924af946b1f49c36efe1c91c6e8e3e606ab
IanWorldHi <ianchensmiley@gmail.com>
2026-04-21 10:41:44 -0400

    express skeleton routes

--- server/index.js
+++ server/index.js
@@ -6,11 +6,35 @@ const PORT = 3001;
 
 app.use(express.json());
 
+//For score CRUD operations
+app.route('/scores/:id')
+    .get((req, res) => {
+        const id = req.params.id;
+    })
+    .post((req, res) => {
+        const id = req.params.id;
+    })
+    .put((req, res) => {
+        const id = req.params.id;
+    })
+    .delete((req, res) => {
+        const id = req.params.id;
+    });
 
-app.get('/scores:id', (req, res) => {
-    const id = req.params.id;
-    
-});
+//For user CRUD operations
+app.route('/users/:id')
+    .get((req, res) => {
+        const id = req.params.id;
+    })
+    .post((req, res) => {
+        const id = req.params.id;
+    })
+    .put((req, res) => {
+        const id = req.params.id;
+    })
+    .delete((req, res) => {
+        const id = req.params.id;
+    });
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
