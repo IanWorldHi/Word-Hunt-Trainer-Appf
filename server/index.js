6eefd9c0788d3cdf495574de39c01bfa0a713a90
IanWorldHi <ianchensmiley@gmail.com>
2026-04-21 09:53:27 -0400

    installing babel

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ const PORT = 3001;
 app.use(express.json());
 
 
+app.get('/scores:id', (req, res) => {
+    const id = req.params.id;
+    
+});
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send("Somethign went wrong");
